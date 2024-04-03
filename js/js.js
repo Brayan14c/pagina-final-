5707633cbf82f1e3101b05e39c46054ea18b5f19
Brayan14c <159051693+Brayan14c@users.noreply.github.com>
2024-04-03 09:58:48 -0500

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,13 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) { // Cambia 50 por la cantidad de desplazamiento que desees antes de que se active la transparencia
+      navbar.classList.add('navbar-scrolled');
+    } else {
+      navbar.classList.remove('navbar-scrolled');
+    }
+  });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const normativityLinks = document.querySelector('#normativity-links.normativity-links');
+    normativityLinks.classList.toggle('animate');
+});
